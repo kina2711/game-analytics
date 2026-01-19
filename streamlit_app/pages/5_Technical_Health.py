9101af52b7e9a7ff00b99e3f75858986e7032b7b
Kien Thai <rabbit.medical2k1@gmail.com>
2026-01-20 01:16:18 +0700

    Update 5_Technical_Health.py

--- streamlit_app/pages/5_Technical_Health.py
+++ streamlit_app/pages/5_Technical_Health.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from data_utils import load_all_data
 from chart_factory import styled_fig
 from config import apply_theme, COLORS
